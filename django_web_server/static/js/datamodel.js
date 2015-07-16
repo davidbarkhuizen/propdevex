a83766b7d46de82d627bfc433f6b96c0d93b1439
david.barkhuizen@gmail.com <david.barkhuizen@gmail.com>
2015-07-16 07:08:06 +0200

    home screen category list links

--- django_web_server/static/js/datamodel.js
+++ django_web_server/static/js/datamodel.js
@@ -9,4 +9,15 @@ function DataModel() {
 			return item.category === categoryName;
 		};
 	};
+
+	that.categoryViews = [
+		{ category: "commercial", view: Views.COMMERCIAL },
+		{ category: "industrial", view: Views.INDUSTRIAL },
+		{ category: "residential", view: Views.RESIDENTIAL },
+		{ category: "business", view: Views.BUSINESS },
+		{ category: "hotel", view: Views.HOTEL },
+		{ category: "retail", view: Views.RETAIL },
+		{ category: "investment", view: Views.INVESTMENT },
+		{ category: "sold", view: Views.SOLD },
+	];
 }
