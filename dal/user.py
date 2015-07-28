91d1d6feb1b8adb2cc64f714ff93721397973066
david.barkhuizen@gmail.com <david.barkhuizen@gmail.com>
2015-07-28 17:40:55 +0200

    django settings

---
+++ dal/user.py
@@ -0,0 +1,64 @@
+from dbconnection import DBConnection
+
+class User(object):
+
+	@classmethod
+	def truncate(cls):
+		db = DBConnection.get_connection()
+		db.execute('truncate table "user";')
+		db.terminate()
+
+	@classmethod
+	def increment_activation_token_distribution_try_count_for_email(cls, email):
+
+		sql = '''
+		update "user"
+		set activation_token_distribution_try_count = activation_token_distribution_try_count + 1
+		where email = '{0}'
+		;'''.format(str(email))
+
+		db_connxn = DBConnection.get_connection()
+		print(sql)	
+		db_connxn.execute(sql)
+		db_connxn.close()
+
+	@classmethod
+	def set_activation_token_distributed_for_email(cls, email):
+
+		sql = '''
+		update "user"
+		set activation_token_distributed = now()
+		where (email = '{0}') 
+		;'''.format(email)
+
+		db_connxn = DBConnection.get_connection()
+		db_connxn.execute(sql)
+		db_connxn.close()
+
+	@classmethod
+	def select_emailuuid_for_undistributed(cls, max_retry_count = 3):
+
+		sql = '''
+		select email, uuid 
+		from "user"
+		where
+			(
+			(activation_token_distributed is null)
+			and
+			(activation_token_distribution_try_count < {0})
+			)
+		;'''.format(str(max_retry_count))
+
+		db_connxn = DBConnection.get_connection()
+
+		data = []
+		rows = db_connxn.fetchall(sql)
+		for row in rows:
+			
+			email = row[0]
+			uuid = row[1]
+
+			datum =  { 'uuid' : uuid, 'email' : email }
+			data.append(datum)
+
+		return data
--- django_web_server/webserver/settings.py
+++ django_web_server/webserver/settings.py
@@ -1,50 +1,41 @@
+import os
+import json
+
+config = json.load(open('/var/www/propdevex/config.json'))
+
+# --------------
+
+SECRET_KEY = config['django_secret_key']
+APPLICATION_ENTRY_POINT = config['application_entry_point']
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+ALLOWED_HOSTS = []
+ROOT_URLCONF = 'webserver.urls'
+WSGI_APPLICATION = 'webserver.wsgi.application'
+
+# DIRECTORIES -------------------------------------------------------
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 MEDIA_ROOT = '/var/www/propdevex/media'
 MEDIA_URL = '/MEDIA/'
 
-"""
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5t^$1ig@+e5rmizoktq25g^8#ooiimi%)fm3vtuzacxi)hki=%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 # django 1.6
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-#
-'''
-# django 1.7+
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-'''
+STATIC_URL = '/static/'
 
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # Application definition
 
@@ -68,39 +59,12 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'webserver.urls'
-
-WSGI_APPLICATION = 'webserver.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/var/www/propdevex/propdevex.sqlite3',
+        'ENGINE': config['db_engine'],
+        'NAME': config['db_name'],
+        'USER' : config['db_user'],
+        'PASSWORD' : config['db_password'],
+        'HOST' : config['db_host']
     }
 }
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
