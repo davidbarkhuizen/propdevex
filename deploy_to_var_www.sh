73dbbe92bedc11e6a7f53ec243bd81e0d0dda22a
david.barkhuizen@gmail.com <david.barkhuizen@gmail.com>
2015-08-23 17:28:14 +0200

    bugfixes

--- deploy_to_var_www.sh
+++ deploy_to_var_www.sh
@@ -5,14 +5,22 @@ sudo mkdir /var/www/sdmm/media/frp
 sudo mkdir /var/www/sdmm/media/frp/image
 sudo mkdir /var/www/sdmm/media/frp/json
 
+
 sudo cp -f config.json.template /var/www/sdmm/config.json.template
 
+sudo rm -Rf /var/www/sdmm/django_web_server
 sudo cp -Rf django_web_server /var/www/sdmm/django_web_server
 
-sudo cp -Rf import /var/www/sdmm/
+sudo rm -Rf /var/www/sdmm/import
+sudo cp -Rf import /var/www/sdmm/import
 
+sudo rm -Rf /var/www/sdmm/dal
 sudo cp -Rf dal /var/www/sdmm/dal
+
+sudo rm -Rf /var/www/sdmm/siteupdater
 sudo cp -Rf siteupdater /var/www/sdmm/siteupdater
+
+sudo rm -Rf /var/www/sdmm/import
 sudo cp -Rf import /var/www/sdmm/import
 
 sudo cp *.py /var/www/sdmm/
