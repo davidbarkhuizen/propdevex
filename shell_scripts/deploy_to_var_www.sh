94f86dc831a61297af4983ceb3a4f944714a7d30
david.barkhuizen@gmail.com <david.barkhuizen@gmail.com>
2015-08-23 16:55:58 +0200

    shell cp fix

--- shell_scripts/deploy_to_var_www.sh
+++ shell_scripts/deploy_to_var_www.sh
@@ -7,13 +7,13 @@ sudo mkdir /var/www/sdmm/media/frp/json
 
 sudo cp ../config.json.template /var/www/sdmm/config.json.template
 
-sudo cp -R ../django_web_server /var/www/sdmm/django_web_server
+sudo cp -Rf ../django_web_server /var/www/sdmm/django_web_server
 
-sudo cp -R ../import /var/www/sdmm/
+sudo cp -Rf ../import /var/www/sdmm/
 
-sudo cp -R ../dal /var/www/sdmm/dal
-sudo cp -R ../siteupdater /var/www/sdmm/siteupdater
-sudo cp -R ../import /var/www/sdmm/import
+sudo cp -Rf ../dal /var/www/sdmm/dal
+sudo cp -Rf ../siteupdater /var/www/sdmm/siteupdater
+sudo cp -Rf ../import /var/www/sdmm/import
 
 sudo cp ../*.py /var/www/sdmm/
 
